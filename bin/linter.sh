ab72cccb0a47eac218e82e2ce51fef0a80746f3a
hbernaciak <hubert.bernaciak@appverk.com>
2020-03-08 23:32:44 +0100

    fixed path

--- bin/linter.sh
+++ bin/linter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "PHP CS FIXER START..."
-vendor/bin/php-cs-fixer --diff --using-cache=no --config=vendor/appverk/symfony-qct/php_cs.dist --verbose --show-progress=estimating fix
+vendor/bin/php-cs-fixer --diff --using-cache=no --config=vendor/appverk/symfony-cqt/php_cs.dist --verbose --show-progress=estimating fix
 echo "...PHP CS FIXER DONE"
 echo "PHPSTAN START..."
-vendor/bin/phpstan analyse --configuration=vendor/appverk/symfony-qct/phpstan.neon --level=max --memory-limit=512M src tests
+vendor/bin/phpstan analyse --configuration=vendor/appverk/symfony-cqt/phpstan.neon --level=max --memory-limit=512M src tests
 echo "...PHPSTAN DONE"
